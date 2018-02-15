5ac6cb5dc7ab24fcb2e316d82305d0ff44312e1c
Taillook <jousoku@gmail.com>
2018-02-15 16:21:15 +0900

    init

---
+++ SlideMenu/SlideMenu.h
@@ -0,0 +1,19 @@
+//
+//  SlideMenu.h
+//  SlideMenu
+//
+//  Created by Taillook on 2018/02/15.
+//  Copyright © 2018年 Taillook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlideMenu.
+FOUNDATION_EXPORT double SlideMenuVersionNumber;
+
+//! Project version string for SlideMenu.
+FOUNDATION_EXPORT const unsigned char SlideMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlideMenu/PublicHeader.h>
+
+
