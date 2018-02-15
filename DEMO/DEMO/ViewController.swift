078679c05408de97c5cd958927ef8658ef7c3c15
Taillook <jousoku@gmail.com>
2018-02-15 17:48:50 +0900

    [ADD] scrollview

--- DEMO/DEMO/ViewController.swift
+++ DEMO/DEMO/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: SlideMenu {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ SlideMenu/SlideMenu.swift
@@ -0,0 +1,37 @@
+//
+//  SlideMenu.swift
+//  SlideMenu
+//
+//  Created by Taillook on 2018/02/15.
+//  Copyright © 2018年 Taillook. All rights reserved.
+//
+
+import UIKit
+
+open class SlideMenu: UIViewController {
+    public var MainView = UIViewController()
+    public var MenuView = UIViewController()
+    public var scrollView = UIScrollView()
+    
+    override open func viewDidLoad() {
+        setUP()
+        addChildViewController(MainView)
+        MainView.view.backgroundColor = UIColor.red
+        MainView.view.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        addChildViewController(MenuView)
+        MenuView.view.backgroundColor = UIColor.blue
+        MenuView.view.frame = CGRect(x: view.bounds.width, y: 0, width: 200, height: view.bounds.height)
+        scrollView.addSubview(MainView.view)
+        scrollView.addSubview(MenuView.view)
+    }
+    
+    func setUP() {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        scrollView.setContentOffset(scrollView.contentOffset, animated: false)
+        scrollView.contentSize = CGSize(width: view.bounds.width + 200, height: view.bounds.height)
+        scrollView.isScrollEnabled = true
+        scrollView.isPagingEnabled = false
+        scrollView.bounces = false
+        view.addSubview(scrollView)
+    }
+}
